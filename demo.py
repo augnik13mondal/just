2c3080253d941a7917c9802cebdd04fdab8a3800
augnik1mondal <159910911+augnik1mondal@users.noreply.github.com>
2024-02-13 19:06:41 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-# This program adds two numbers
-
 num1 = 2
-num2 = 3
-
-# Add two numbers
+num2 = 4
 sum = num1 + num2
-
-# Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
